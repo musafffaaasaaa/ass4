b30b029f407f1abb5dd8818889d70210f4326c3c
musafffaaasaaa <97581061+musafffaaasaaa@users.noreply.github.com>
2022-11-03 23:33:51 +0600

    Add files via upload

---
+++ boxforgit/cmd/web/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"boxforgit.musawork.net/internal/models"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/go-playground/form/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type application struct {
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	os.Setenv("dsn", "postgres://postgres:admin@localhost:5432/goland")
+
+	dsn := os.Getenv("dsn")
+	fmt.Println(dsn)
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Store = pgxstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
+	app := &application{
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
